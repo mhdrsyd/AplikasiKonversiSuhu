8deaf808215764879c7013270b9d32009595c46b
mhdrsyd <muhammadrasyid5654@gmail.com>
2025-10-21 21:05:34 +0800

    577

--- src/konversiSuhu.java
+++ src/konversiSuhu.java
@@ -39,7 +39,6 @@ public class konversiSuhu extends javax.swing.JFrame {
 
         buttonGroup1 = new javax.swing.ButtonGroup();
         jPanel1 = new javax.swing.JPanel();
-        judul = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
         cbmSatuan = new javax.swing.JComboBox<>();
         txtsuhu = new javax.swing.JTextField();
@@ -50,17 +49,17 @@ public class konversiSuhu extends javax.swing.JFrame {
         r4Reamur = new javax.swing.JRadioButton();
         btnHasil = new javax.swing.JButton();
         txtHasil = new javax.swing.JTextField();
+        jPanel2 = new javax.swing.JPanel();
+        judul = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setResizable(false);
 
+        jPanel1.setBackground(new java.awt.Color(0, 204, 102));
         jPanel1.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
 
-        judul.setBackground(new java.awt.Color(0, 0, 0));
-        judul.setFont(new java.awt.Font("Times New Roman", 1, 24)); // NOI18N
-        judul.setText("APLIKASI KONVERSI SUHU");
-
-        jLabel2.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        jLabel2.setFont(new java.awt.Font("Segoe UI", 3, 18)); // NOI18N
+        jLabel2.setForeground(java.awt.Color.white);
         jLabel2.setText("Masukkan Suhu Dalam");
 
         cbmSatuan.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Celcius(°C)", "Reamur(°R)", "Fahrenheit(°H)", "Kelvin(°K)" }));
@@ -71,6 +70,7 @@ public class konversiSuhu extends javax.swing.JFrame {
         });
 
         txtsuhu.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        txtsuhu.setHorizontalAlignment(javax.swing.JTextField.CENTER);
         txtsuhu.addFocusListener(new java.awt.event.FocusAdapter() {
             public void focusLost(java.awt.event.FocusEvent evt) {
                 txtsuhuFocusLost(evt);
@@ -85,15 +85,18 @@ public class konversiSuhu extends javax.swing.JFrame {
             }
         });
 
-        jLabel3.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        jLabel3.setFont(new java.awt.Font("Segoe UI", 3, 18)); // NOI18N
+        jLabel3.setForeground(java.awt.Color.white);
         jLabel3.setText("Konversi Ke Dalam ");
 
         buttonGroup1.add(r1Fahrenheit);
-        r1Fahrenheit.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
+        r1Fahrenheit.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        r1Fahrenheit.setForeground(java.awt.Color.white);
         r1Fahrenheit.setText("Fahrenheit");
 
         buttonGroup1.add(r2Celcius);
-        r2Celcius.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
+        r2Celcius.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        r2Celcius.setForeground(java.awt.Color.white);
         r2Celcius.setText("Celcius");
         r2Celcius.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -102,11 +105,13 @@ public class konversiSuhu extends javax.swing.JFrame {
         });
 
         buttonGroup1.add(r3Kelvin);
-        r3Kelvin.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
+        r3Kelvin.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        r3Kelvin.setForeground(java.awt.Color.white);
         r3Kelvin.setText("kelvin");
 
         buttonGroup1.add(r4Reamur);
-        r4Reamur.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
+        r4Reamur.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        r4Reamur.setForeground(java.awt.Color.white);
         r4Reamur.setText("Reamur");
 
         btnHasil.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
@@ -119,35 +124,62 @@ public class konversiSuhu extends javax.swing.JFrame {
 
         txtHasil.setEditable(false);
         txtHasil.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        txtHasil.setHorizontalAlignment(javax.swing.JTextField.CENTER);
         txtHasil.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 txtHasilActionPerformed(evt);
             }
         });
 
+        jPanel2.setBackground(new java.awt.Color(0, 153, 102));
+
+        judul.setBackground(new java.awt.Color(0, 0, 0));
+        judul.setFont(new java.awt.Font("Times New Roman", 1, 24)); // NOI18N
+        judul.setForeground(new java.awt.Color(255, 255, 0));
+        judul.setText("APLIKASI KONVERSI SUHU");
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(judul)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(judul)
+                .addContainerGap(18, Short.MAX_VALUE))
+        );
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addGap(20, 20, 20)
+                .addComponent(cbmSatuan, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGap(18, 18, 18)
+                .addComponent(txtsuhu, javax.swing.GroupLayout.PREFERRED_SIZE, 142, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(27, 27, 27))
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(159, 159, 159)
-                        .addComponent(jLabel3))
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(29, 29, 29)
-                        .addComponent(jLabel2)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(cbmSatuan, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(18, 18, 18)
-                        .addComponent(txtsuhu, javax.swing.GroupLayout.PREFERRED_SIZE, 75, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGap(62, 62, 62)
-                        .addComponent(judul)))
-                .addContainerGap(59, Short.MAX_VALUE))
-            .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 207, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(78, 78, 78)
+                        .addComponent(jLabel3)))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                 .addGap(0, 0, Short.MAX_VALUE)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(r2Celcius)
@@ -156,26 +188,23 @@ public class konversiSuhu extends javax.swing.JFrame {
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(r4Reamur)
                             .addComponent(r3Kelvin)))
-                    .addComponent(txtHasil, javax.swing.GroupLayout.PREFERRED_SIZE, 209, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(0, 0, Short.MAX_VALUE))
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                .addGap(0, 0, Short.MAX_VALUE)
-                .addComponent(btnHasil, javax.swing.GroupLayout.PREFERRED_SIZE, 127, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(160, 160, 160))
+                    .addComponent(btnHasil, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(txtHasil))
+                .addGap(61, 61, 61))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(31, 31, 31)
-                .addComponent(judul)
-                .addGap(47, 47, 47)
+                .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 31, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(17, 17, 17)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel2)
-                    .addComponent(cbmSatuan, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(txtsuhu, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(38, 38, 38)
+                    .addComponent(cbmSatuan, javax.swing.GroupLayout.PREFERRED_SIZE, 29, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(txtsuhu, javax.swing.GroupLayout.PREFERRED_SIZE, 29, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
                 .addComponent(jLabel3)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGap(18, 18, 18)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(r2Celcius)
                     .addComponent(r3Kelvin))
@@ -187,16 +216,14 @@ public class konversiSuhu extends javax.swing.JFrame {
                 .addComponent(btnHasil)
                 .addGap(18, 18, 18)
                 .addComponent(txtHasil, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(40, Short.MAX_VALUE))
+                .addContainerGap(37, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 0, Short.MAX_VALUE))
+            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 335, javax.swing.GroupLayout.PREFERRED_SIZE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -408,6 +435,7 @@ public class konversiSuhu extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
     private javax.swing.JLabel judul;
     private javax.swing.JRadioButton r1Fahrenheit;
     private javax.swing.JRadioButton r2Celcius;
